ae9465c8e22afdf1b4c5f9514e9a1336ed0244f4
zhangduanjian <199818zj>
2022-09-10 09:06:09 +0800

    update .gitignore

--- router/auth.js
+++ router/auth.js
@@ -5,9 +5,11 @@ const Router = require('koa-router');
 
 const authRoute = new Router();
 
-const { login } = require('../controller/authController.js');
-const { verifyLogin } = require('../middleware/authMid');
+const { login, suceed } = require('../controller/authController.js');
+const { verifyLogin, verifyAuth } = require('../middleware/authMid');
 
 authRoute.post('/login', verifyLogin, login);
+// 验证授权接口
+authRoute.get('/test', verifyAuth, suceed);
 
 module.exports = authRoute;
